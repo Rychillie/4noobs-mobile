72f711359da519e7ea2d60db0c258348889d3b32
Rychillie <rychillie@hotmail.com>
2021-02-07 16:31:37 -0300

    refatorando tudo

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-
-import App from "./src/index";
-
-export default function Main() {
-  return <App />;
-}
--- src/navigation/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import MainBottomTab from './MainBottomTab.routes';
-
-export default function Routes() {
-  return <MainBottomTab />;
-}
